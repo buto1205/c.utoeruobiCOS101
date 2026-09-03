3f8eab36e48090a2bab92a52caccc56f1b86dde1
buto1205 <chizaram.utoeruobi@pau.edu.ng>
2026-09-03 19:30:08 +0100

    my second commit

---
+++ week-2/practice_2.rs
@@ -0,0 +1,6 @@
+fn main() {
+println!(); //prints just a
+println!("hello");//prints
+println!("format {} arguments", "some"); //prints format some arguments
+println!("my name is {}. i study software engineering", "Utoeruobi Benedicta-Chizaram" );	
+}
