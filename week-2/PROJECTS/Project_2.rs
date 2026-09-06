39cf019a7e7b7e029cccc50d87b20d6ae7391d23
buto1205 <chizaram.utoeruobi@pau.edu.ng>
2026-09-06 14:40:01 +0100

    my second commit

---
+++ week-2/PROJECTS/Project_2.rs
@@ -0,0 +1,13 @@
+fn main() {
+let sum = 450_000.00*2.00 + 1_500_000.00 + 750_000.00*3.00 + 2_850_000.00*3.00 + 250_000.00;
+let avg = sum / 10.00;
+println!("The sum and average of the sales are{} and {} respectively",sum,avg );
+}
+
+
+
+
+
+
+
+
